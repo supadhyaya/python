2df5312ef85c3efc1f5a24c02c613096c6b3fa56
supadhyaya <sanjiv.upadhyaya@tum.de>
2016-04-22 12:29:54 +0200

    new files from the practice session

--- euler_1.py
+++ euler_1.py
@@ -1,5 +1,7 @@
 
 
+
+
 """
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
@@ -17,20 +19,23 @@ multiplier1 = int(input("what is the first multiplier\n"))
 multiplier2 = int(input("what is the second multiplier\n"))
 
 
-result1 = 0
-result2 = 0
+result1 = set()
+final_result  = 0
+
 
 for i_1 in range(multiplier1, number):
     if(i_1 % multiplier1 == 0):
-        result1 = result1 + i_1
+        result1.add(i_1)
 
 for i_2 in range(multiplier2, number):
     if(i_2 % multiplier2 == 0):
-        result2 = result2 + i_2
-
-print("Result: %s" %(result1+result2))
+        result1.add(i_2)
 
 print(result1)
-print(result2)
+
+for x in result1:
+    final_result   = final_result + x
+
+print(final_result)
 
 
