3dbbc2602afbb7166c7ae726aa434c1ccc9d3169
supadhyaya <sanjiv.upadhyaya@tum.de>
2016-04-22 14:00:15 +0200

    new files from the practice session

--- euler_1.py
+++ euler_1.py
@@ -4,36 +4,52 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-#This problem will give an overview of the logical error while programming.
+# This problem will give an overview of the logical error while programming.
+
+multiplier = set()  # Store the use given multipliers in a set
+result = set()      # Add all the numbers which produces 0 as remainder
+final_result = 0    # Holds sum of all the numbers from the set named result
+mul_cnt = True      # Counter to track the inputs from the user
 
 
-number = int(input("please enter a number you want to divide\n"))  #Number whose multiple we are going to add
+#  Number whose multiple we are going to add
+try:
+    number = int(input("please enter a number you want to divide\n"))
 
-multiplier = set() #Store the use given multipliers in a set
-result = set()     #Add all the numbers which produces 0 as remainder
-final_result = 0   #Holds sum of all the numbers from the set named result
-mul_cnt = True     #Counter to track the inputs from the user
+except ValueError:
+    print("Oopsy !! Enter a valid integer")
 
-# Ask the user for the multipliers they want to use
-while mul_cnt == True:
-    x = int(input("Please enter the multipliers you want to use. Press 0 to end the input\n"))
-    if (x != 0):
-        multiplier.add(x)
-    else:
-        mul_cnt = False
 
-# Add all the multipliers to a single tuple called result
+#  Ask the user for the multipliers they want to use
+while mul_cnt:
+    try:
+        x = int(input("Please enter the multipliers you want to use. Press 0 to end the input\n"))
+
+        if x != 0:
+            multiplier.add(x)
+        else:
+            mul_cnt = False
+
+            break
+
+    except ValueError:
+        print("Oppsy !! Enter a valid integer")
+
+
+#  Add all the multipliers to a single tuple called result
 for mul in multiplier:
-    for num in range(mul,number):
-        if(num % mul == 0):
+    # noinspection PyUnboundLocalVariable
+    for num in range(mul, number):
+        if num % mul == 0:
             result.add(num)
 
 
-# Add all the elements of the tuple to print the final result
+#  Add all the elements of the tuple to print the final result
 for items in result:
     final_result = final_result + items
 
-print("Answer: %s" %final_result)
+
+print("Answer: %s" % final_result)
 
 
 
